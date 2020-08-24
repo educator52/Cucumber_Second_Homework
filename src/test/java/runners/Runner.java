36eb00a70a0f30e483a94e1fd2b6cd09d8a9f0e0
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2020-08-24 14:54:16 +0200

    Runner added.

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -1,10 +1,21 @@
 package runners;
 
+import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
 import utils.BaseDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.apache.commons.io.FileUtils.copyFile;
 
 @CucumberOptions(
         plugin = {
@@ -20,6 +31,15 @@ public class Runner extends AbstractTestNGCucumberTests {
     @AfterClass
     public void quitDriver(){
         BaseDriver.quitDriver();
+    }
+    @AfterClass
+    public void configReport(){
+        Reporter.loadXMLConfig("src/test/java/runners/reportConfig/report.xml");
+        Reporter.setSystemInfo("Author", "Simple");
+        Reporter.setSystemInfo("Application name", "Simple");
+        Reporter.setSystemInfo("Operating System", System.getProperty("os.name"));
+        Reporter.setSystemInfo("Environment", "test");
+
     }
 
 
